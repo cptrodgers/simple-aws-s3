acf15d39d28b5ad774020d3409b1e9aeaae2d9d2
Rodgers <cptrodgers@gmail.com>
2021-06-01 22:57:22 +0700

    Init, S3 Signer

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple AWS S3
+Provides the simple way to work with AWS S3. This package is developing while
+waiting the supported from aws-sdk from Amazon.
+
+Currently, aws-sdk are not ready for s3 (because they need to support many services).
+I hope this crate can help you to the easy way to work with s3 in this time.
+
+# LICENSE
+
+<sup>
+Licensed under either of <a href="LICENSE-APACHE">Apache License, Version
+2.0</a> or <a href="LICENSE-MIT">MIT license</a> at your option.
+</sup>
+
+<br>
+
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in Serde by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+</sub>
---
+++ src/s3_signer.rs
@@ -0,0 +1,52 @@
+use chrono::{DateTime, Utc};
+use hmac::crypto_mac::InvalidKeyLength;
+use hmac::{Hmac, Mac, NewMac};
+use sha2::Sha256;
+
+type HmacSha256 = Hmac<Sha256>;
+
+pub struct Signer<'s> {
+    secret_key: &'s str,
+    region: &'s str,
+}
+
+impl<'s> Signer<'s> {
+    #[inline]
+    pub fn new(secret_key: &'s str, region: &'s str) -> Self {
+        Self { secret_key, region }
+    }
+
+    #[inline]
+    pub fn sign(&self, date: DateTime<Utc>, string_to_sign: &str) -> Result<String, InvalidKeyLength> {
+        let mut key = self.signing_hasher(date)?;
+        key.update(string_to_sign.as_bytes());
+        let msg = key.finalize().into_bytes();
+        Ok(hex::encode(&msg))
+    }
+
+    #[inline]
+    fn signing_hasher(&self, date: DateTime<Utc>) -> Result<HmacSha256, InvalidKeyLength> {
+        // Step 1: Sign Date
+        let date = date.format("%Y%m%d").to_string();
+        let mut h = HmacSha256::new_from_slice(format!("AWS4{}", self.secret_key).as_bytes())?;
+        h.update(date.as_bytes());
+        let date_key = h.finalize().into_bytes();
+
+        // Step 2: Sign Date and Region
+        let mut date_region_h = HmacSha256::new_from_slice(&date_key)?;
+        date_region_h.update(self.region.as_bytes());
+        let date_region_key = date_region_h.finalize().into_bytes();
+
+        // Step 3: Sign Date and region and Service
+        let mut date_region_service_h = HmacSha256::new_from_slice(&date_region_key)?;
+        date_region_service_h.update(b"s3");
+        let date_region_service_key = date_region_service_h.finalize().into_bytes();
+
+        // Step 4: Final sign
+        let mut signing_key_h = HmacSha256::new_from_slice(&date_region_service_key)?;
+        signing_key_h.update(b"aws4_request");
+        let signing_key = signing_key_h.finalize().into_bytes();
+
+        HmacSha256::new_from_slice(&signing_key)
+    }
+}
